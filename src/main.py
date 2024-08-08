e7f480c3ad91d3b751f9fc4588a532254ec2b995
Efi Poulea <epp@cmm.gr>
2024-08-08 16:00:33 +0300

    add new lines

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ def main():
     hello("world!")
     print("Commit changes!!")
     goodbye("thank you!")
+    hello("hello there!")
+    goodbye("thank you for watching!")
 
 if __name__ == "__main__":
     main()
